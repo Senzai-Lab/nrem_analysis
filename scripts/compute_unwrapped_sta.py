22e0b00d876b39d4944a652e61b235c4aab51865
Tuguldur Gerelmaa <tuguldur.gerelmaa@northwestern.edu>
2026-08-07 16:19:25 -0500

    add upstate detection functionality

--- scripts/compute_unwrapped_sta.py
+++ scripts/compute_unwrapped_sta.py
@@ -7,35 +7,45 @@ import pynapple as nap
 
 MOUSE_IDS_DUAL = ["99b", "103c", "106b", "107b", "110b"]
 
-AWAKE_STA_KWARGS = dict(
-    bin_size=10,
-    window=(-500, 500),
+NREM_UPSTATE = dict(
+    bin_size=2,
+    window=(-501, 501),
     time_unit="ms",
 )
 
-NREM_STA_KWARGS = dict(
-    bin_size=2,
-    window=(-100, 100),
-    time_unit="ms",
-)
+def detect_upstates(
+    spikes: nap.TsGroup,
+    bin_size = 0.01,
+    percentile = 80,
+    merge_thr = 0.1,
+    short_thr = 0.04,
+    long_thr = 2,
+    time_units='s'
+    ) -> nap.IntervalSet:
+    x = spikes.count(bin_size=bin_size, time_units=time_units).sum(axis=1)
+    x = x / bin_size
+    x = x.smooth(std=2 * bin_size, time_units=time_units)
+    thr = np.percentile(x, q=percentile)
+
+    up_states = x.threshold(thr, method='above').time_support
+    up_states = up_states.merge_close_intervals(threshold=merge_thr, time_units=time_units)
+    up_states = up_states.drop_short_intervals(threshold=short_thr, time_units=time_units)
+    up_states = up_states.drop_long_intervals(threshold=long_thr, time_units=time_units)
+    return up_states, thr
 
 def unwrap_epochs(data):
-    values = data.d.copy()
-    times = data.t
+    d = data.d.copy()
+    t = data.t
 
     for start, end in zip(
         data.time_support.start,
         data.time_support.end,
     ):
-        first = np.searchsorted(times, start, side="left")
-        last = np.searchsorted(times, end, side="right")
-        values[first:last] = np.unwrap(values[first:last])
+        i = np.searchsorted(t, start, side="left")
+        j = np.searchsorted(t, end, side="right")
+        d[i:j] = np.unwrap(d[i:j])
 
-    return nap.Tsd(
-        t=times,
-        d=values,
-        time_support=data.time_support,
-    )
+    return nap.Tsd(t=t, d=d, time_support=data.time_support,)
 
 def compute_unwrapped_sta(
     data,
@@ -45,34 +55,45 @@ def compute_unwrapped_sta(
     time_unit,
     fill_value=np.nan,
 ):
-    unit_stas = []
+    stas = []
+    times = None
+
     for uid in units.index:
         events = units[uid].restrict(data.time_support)
 
         if len(events) == 0:
             print(f"Filling unit {uid} with {fill_value}")
-            unit_stas.append(None)
+            stas.append(None)
             continue
 
-        result = nap.compute_perievent(data=data, events=events, window=window, time_unit=time_unit,)
-        # remove baseline
-        zero_idx = np.argmin(np.abs(result.index))
-        result = result - result[zero_idx]
-        result = np.nanmean(result, axis=1)
-        result = result.bin_average(bin_size=bin_size, time_units=time_unit)
+        perievent = nap.compute_perievent(
+            data=data,
+            events=events,
+            window=window,
+            time_unit=time_unit,
+        )
 
-        unit_stas.append(result.d)
+        sta = np.nanmean(perievent, axis=1)
+        sta = sta.bin_average(bin_size=bin_size, time_units=time_unit,)
+        zero_idx = np.argmin(np.abs(sta.index))
+        sta = sta - sta[zero_idx]
 
-    unit_stas = [
-        np.full(result.t.shape, fill_value) if sta is None else sta
-        for sta in unit_stas
+        times = sta.t
+        stas.append(sta.d)
+
+    if times is None:
+        raise ValueError("No units have events within data.time_support.")
+
+    stas = [
+        np.full(times.shape, fill_value) if sta is None else sta
+        for sta in stas
     ]
 
     return nap.TsdFrame(
-        t=result.t,
-        d=np.column_stack(unit_stas),
+        t=times,
+        d=np.column_stack(stas),
         columns=units.index,
-        )
+    )
 
 def main(
     input_path: str,
@@ -114,30 +135,26 @@ def main(
         print(f"Data directory: {data_dir}")
         print("-" * 100)
 
+        hd_units        = nap.load_file(data_dir / "hd_units.npz")
         turn_units      = nap.load_file(data_dir / "turn_units.npz")
-        head_direction  = nap.load_file(data_dir / "head_direction.npz")
-        sweeps          = nap.load_file(data_dir / "sweeps.npz")
-        
-        print("Computing awake STA")
-        sta_awake = compute_unwrapped_sta(
-            data=unwrap_epochs(np.deg2rad(head_direction)),
-            units=turn_units,
-            **AWAKE_STA_KWARGS,
-        )
+        sleep           = nap.load_file(data_dir / "sleep.npz")
+        session         = nap.load_file(data_dir / "session.npz")
+        virtual_hd      = nap.load_file(data_dir / "virtual_hd.npz")
 
-        awake_output = save_dir / 'sta_awake.npz'
-        print(f"Saving awake STA: {awake_output}")
-        sta_awake.save(awake_output)
+        nrem            = sleep[sleep['state'] == 'nrem'].intersect(session[session['state'] == 'homecage'])
+        print("Detecting upstates")
+        upstate, _ = detect_upstates(hd_units.restrict(nrem))
+        upstate_decoded = virtual_hd.restrict(upstate)
 
-        print("Computing continuous-NREM STA")
+        print("Computing NREM STA")
         sta_nrem = compute_unwrapped_sta(
-            data=unwrap_epochs(np.deg2rad(sweeps)),
+            data=unwrap_epochs(np.deg2rad(upstate_decoded)),
             units=turn_units,
-            **NREM_STA_KWARGS,
+            **NREM_UPSTATE,
         )
 
-        nrem_output = save_dir / 'sta_nrem.npz'
-        print(f"Saving continuous-NREM STA: {nrem_output}")
+        nrem_output = save_dir / 'sta_nrem_upstate.npz'
+        print(f"Saving NREM STA: {nrem_output}")
         sta_nrem.save(nrem_output)
 
         print(f"Finished mouse: {mouse_id}")
